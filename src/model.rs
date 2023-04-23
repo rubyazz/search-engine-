699b252bd8c66ba66549fd40880e2b862426f34a
rubyazz <ersultan.abduvalov@gmail.com>
2023-04-23 22:09:09 +0600

    models and server done

---
+++ src/model.rs
@@ -0,0 +1,97 @@
+use std::collections::HashMap;
+use std::path::{PathBuf, Path};
+use serde::{Deserialize, Serialize};
+use super::lexer::Lexer;
+use std::time::SystemTime;
+
+type DocFreq = HashMap<String, usize>;
+type TermFreq = HashMap<String, usize>;
+#[derive(Deserialize, Serialize)]
+pub struct Doc {
+    tf: TermFreq,
+    count: usize,
+    // TODO: make sure that the serde serialization of SystemTime also work on other platforms
+    last_modified: SystemTime,
+}
+type Docs = HashMap<PathBuf, Doc>;
+
+#[derive(Default, Deserialize, Serialize)]
+pub struct Model {
+    pub docs: Docs,
+    pub df: DocFreq,
+}
+
+impl Model {
+    fn remove_document(&mut self, file_path: &Path) {
+        if let Some(doc) = self.docs.remove(file_path) {
+            for t in doc.tf.keys() {
+                if let Some(f) = self.df.get_mut(t) {
+                    *f -= 1;
+                }
+            }
+        }
+    }
+
+    pub fn requires_reindexing(&mut self, file_path: &Path, last_modified: SystemTime) -> bool {
+        if let Some(doc) = self.docs.get(file_path) {
+            return doc.last_modified < last_modified;
+        }
+        return true;
+    }
+
+    pub fn search_query(&self, query: &[char]) -> Vec<(PathBuf, f32)> {
+        let mut result = Vec::new();
+        let tokens = Lexer::new(&query).collect::<Vec<_>>();
+        for (path, doc) in &self.docs {
+            let mut rank = 0f32;
+            for token in &tokens {
+                rank += compute_tf(token, doc) * compute_idf(&token, self.docs.len(), &self.df);
+            }
+            // TODO: investigate the sources of NaN
+            if !rank.is_nan() {
+                result.push((path.clone(), rank));
+            }
+        }
+        result.sort_by(|(_, rank1), (_, rank2)| rank1.partial_cmp(rank2).expect(&format!("{rank1} and {rank2} are not comparable")));
+        result.reverse();
+        result
+    }
+
+    pub fn add_document(&mut self, file_path: PathBuf, last_modified: SystemTime, content: &[char]) {
+        self.remove_document(&file_path);
+
+        let mut tf = TermFreq::new();
+
+        let mut count = 0;
+        for t in Lexer::new(content) {
+            if let Some(f) = tf.get_mut(&t) {
+                *f += 1;
+            } else {
+                tf.insert(t, 1);
+            }
+            count += 1;
+        }
+
+        for t in tf.keys() {
+            if let Some(f) = self.df.get_mut(t) {
+                *f += 1;
+            } else {
+                self.df.insert(t.to_string(), 1);
+            }
+        }
+
+        self.docs.insert(file_path, Doc {count, tf, last_modified});
+    }
+}
+
+fn compute_tf(t: &str, doc: &Doc) -> f32 {
+    let n = doc.count as f32;
+    let m = doc.tf.get(t).cloned().unwrap_or(0) as f32;
+    m / n
+}
+
+fn compute_idf(t: &str, n: usize, df: &DocFreq) -> f32 {
+    let n = n as f32;
+    let m = df.get(t).cloned().unwrap_or(1) as f32;
+    (n / m).log10()
+}
---
+++ src/server.rs
@@ -0,0 +1,126 @@
+use std::str;
+use std::io;
+use std::sync::{Arc, Mutex};
+
+use super::model::*;
+
+use tiny_http::{Server, Request, Response, Header, Method, StatusCode};
+
+fn serve_404(request: Request) -> io::Result<()> {
+    request.respond(Response::from_string("404").with_status_code(StatusCode(404)))
+}
+
+fn serve_500(request: Request) -> io::Result<()> {
+    request.respond(Response::from_string("500").with_status_code(StatusCode(500)))
+}
+
+fn serve_400(request: Request, message: &str) -> io::Result<()> {
+    request.respond(Response::from_string(format!("400: {message}")).with_status_code(StatusCode(400)))
+}
+
+fn serve_bytes(request: Request, bytes: &[u8], content_type: &str) -> io::Result<()> {
+    let content_type_header = Header::from_bytes("Content-Type", content_type)
+        .expect("That we didn't put any garbage in the headers");
+    request.respond(Response::from_data(bytes).with_header(content_type_header))
+}
+
+// TODO: the errors of serve_api_search should probably return JSON
+// 'Cause that's what expected from them.
+fn serve_api_search(model: Arc<Mutex<Model>>, mut request: Request) -> io::Result<()> {
+    let mut buf = Vec::new();
+    if let Err(err) = request.as_reader().read_to_end(&mut buf) {
+        eprintln!("ERROR: could not read the body of the request: {err}");
+        return serve_500(request);
+    }
+
+    let body = match str::from_utf8(&buf) {
+        Ok(body) => body.chars().collect::<Vec<_>>(),
+        Err(err) => {
+            eprintln!("ERROR: could not interpret body as UTF-8 string: {err}");
+            return serve_400(request, "Body must be a valid UTF-8 string");
+        }
+    };
+
+    let model = model.lock().unwrap();
+    let result = model.search_query(&body);
+
+    let json = match serde_json::to_string(&result.iter().take(20).collect::<Vec<_>>()) {
+        Ok(json) => json,
+        Err(err) => {
+            eprintln!("ERROR: could not convert search results to JSON: {err}");
+            return serve_500(request)
+        }
+    };
+
+    let content_type_header = Header::from_bytes("Content-Type", "application/json")
+        .expect("That we didn't put any garbage in the headers");
+    request.respond(Response::from_string(&json).with_header(content_type_header))
+}
+
+fn serve_api_stats(model: Arc<Mutex<Model>>, request: Request) -> io::Result<()> {
+    use serde::Serialize;
+
+    #[derive(Default, Serialize)]
+    struct Stats {
+        docs_count: usize,
+        terms_count: usize,
+    }
+
+    let mut stats: Stats = Default::default();
+    {
+        let model = model.lock().unwrap();
+        stats.docs_count = model.docs.len();
+        stats.terms_count = model.df.len();
+    }
+
+    let json = match serde_json::to_string(&stats) {
+        Ok(json) => json,
+        Err(err) => {
+            eprintln!("ERROR: could not convert stats results to JSON: {err}");
+            return serve_500(request)
+        }
+    };
+
+    let content_type_header = Header::from_bytes("Content-Type", "application/json")
+        .expect("That we didn't put any garbage in the headers");
+    request.respond(Response::from_string(&json).with_header(content_type_header))
+}
+
+fn serve_request(model: Arc<Mutex<Model>>, request: Request) -> io::Result<()> {
+    println!("INFO: received request! method: {:?}, url: {:?}", request.method(), request.url());
+
+    match (request.method(), request.url()) {
+        (Method::Post, "/api/search") => {
+            serve_api_search(model, request)
+        }
+        (Method::Get, "/api/stats") => {
+            serve_api_stats(model, request)
+        }
+        (Method::Get, "/index.js") => {
+            serve_bytes(request, include_bytes!("index.js"), "text/javascript; charset=utf-8")
+        }
+        (Method::Get, "/") | (Method::Get, "/index.html") => {
+            serve_bytes(request, include_bytes!("index.html"), "text/html; charset=utf-8")
+        }
+        _ => {
+            serve_404(request)
+        }
+    }
+}
+
+pub fn start(address: &str, model: Arc<Mutex<Model>>) -> Result<(), ()> {
+    let server = Server::http(&address).map_err(|err| {
+        eprintln!("ERROR: could not start HTTP server at {address}: {err}");
+    })?;
+
+    println!("INFO: listening at http://{address}/");
+
+    for request in server.incoming_requests() {
+        serve_request(Arc::clone(&model), request).map_err(|err| {
+            eprintln!("ERROR: could not serve the response: {err}");
+        }).ok(); // <- don't stop on errors, keep serving
+    }
+
+    eprintln!("ERROR: the server socket has shutdown");
+    Err(())
+}
